f51c5489be74420f812d9190331fab34da742e86
micamilano <micaa.milano@gmail.com>
2021-07-15 23:40:52 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const btnForm= document.getElementById('btn-submit');
+
+btnForm.addEventListener('click',handler);
+
+function handler(){
+    alert('hola');
+}
+
+//          action="sendbymail.php"
